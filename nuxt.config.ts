c2430a9946f62fc055ffec4aed5f20f43f4eee50
Andre Saputra <andre.saputra@lolipad.com>
2023-03-05 20:05:47 +0700

    fix error build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,18 +19,7 @@ export default defineNuxtConfig({
       ],
     }
   },
-  modules: ['nuxt-swiper' ,'nuxt-vuefire', 'nuxt-icon'],
-  vuefire: {
-    config: {
-      apiKey: "AIzaSyCxmHYKHx07zYcVq9uWC7QOlLQKeiLdJ0w",
-      authDomain: "fir-test-125e4.firebaseapp.com",
-      projectId: "fir-test-125e4",
-      storageBucket: "fir-test-125e4.appspot.com",
-      messagingSenderId: "1069511782508",
-      appId: "1:1069511782508:web:bd8a626e30568e2f866849",
-      measurementId: "G-JXFQD8F4V2"
-    },
-  },
+  modules: ['nuxt-swiper' ,'nuxt-icon'],
   postcss: {
     plugins: {
       tailwindcss: {},
